8b8b2fc75aa63a49af0ca4bdf3c31f2d0fd51300
root <root@ip-172-31-19-35.ec2.internal>
2017-09-12 03:26:57 +0000

    updating stream

--- resources/analytics/producer.py
+++ resources/analytics/producer.py
@@ -2,12 +2,12 @@
 from kafka import KafkaProducer
 import time
 
-file = "./fraud.topic"
+file = "resources/analytics/fraud.topic"
 producer = KafkaProducer(bootstrap_servers='127.0.0.1:9092')
 while 1:
    with open(file, "r") as topic:
       for msg in topic:
-         print(msg.strip())
+#         print(msg.strip())
          time.sleep(0.5)
          producer.send("fraud", msg.strip())
          producer.flush()
