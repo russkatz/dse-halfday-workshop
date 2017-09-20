017b707366c765c67e37d430c9aa1dc7a9ba2bdd
root <root@ip-172-31-19-35.ec2.internal>
2017-09-20 17:48:53 +0000

    updating workbook update script

--- resources/workbooks/update_worksbooks.sh
+++ resources/workbooks/update_worksbooks.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
+echo "Killing DSE Studio"
+kill -9 `ps -aef | grep studio-server-2.0.0.jar | grep -v grep | awk '{print $2}'`
+sleep 2
+
 rm /tmp/dse-workshop-studio.tar.gz
 rm -rf /opt/dse/datastax-studio-2.0.0/dse-workshop-studio
 wget --no-check-certificate --content-disposition -P /tmp https://github.com/russkatz/dse-halfday-workshop/raw/master/files/ dse-workshop-studio.tar.gz
 cd /opt/dse/datastax-studio-2.0.0/
 tar -zxvf /tmp/dse-workshop-studio.tar.gz
+sleep 2
 
+nohup /opt/dse/datastax-studio-2.0.0/bin/server.sh &
 
