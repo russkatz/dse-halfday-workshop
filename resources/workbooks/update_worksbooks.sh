dddcd87db9697b31fe6ebb368104cb136ab10308
root <root@ip-172-31-19-35.ec2.internal>
2017-09-20 18:31:47 +0000

    update workbooks script fix

--- resources/workbooks/update_worksbooks.sh
+++ resources/workbooks/update_worksbooks.sh
@@ -6,7 +6,7 @@ sleep 2
 
 rm /tmp/dse-workshop-studio.tar.gz
 rm -rf /opt/dse/datastax-studio-2.0.0/dse-workshop-studio
-wget --no-check-certificate --content-disposition -P /tmp https://github.com/russkatz/dse-halfday-workshop/raw/master/files/ dse-workshop-studio.tar.gz
+wget --no-check-certificate --content-disposition -P /tmp https://github.com/russkatz/dse-halfday-workshop/raw/master/files/dse-workshop-studio.tar.gz
 cd /opt/dse/datastax-studio-2.0.0/
 tar -zxvf /tmp/dse-workshop-studio.tar.gz
 sleep 2
